b2b34beb9b4388b36fa7b249014d9cbbf206fdbc
Chris Woodruff <cwoodruff@live.com>
2024-10-22 19:18:42 -0400

    Added code to use SQLite database for Chinook demo

--- chinook-razor-htmx/ChinookHTMX/Configurations/ConfigureConnections.cs
+++ chinook-razor-htmx/ChinookHTMX/Configurations/ConfigureConnections.cs
@@ -9,15 +9,8 @@ public static class ConfigureConnections
     public static IServiceCollection AddConnectionProvider(this IServiceCollection services,
         IConfiguration configuration)
     {
-        var connection = String.Empty;
-
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            connection = configuration.GetConnectionString("ChinookDbWindows");
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            connection = configuration.GetConnectionString("ChinookDbDocker");
-
-        services.AddDbContextPool<ChinookContext>(options => options.UseSqlServer(connection));
-
+        var connection = configuration.GetConnectionString("Chinook");
+        services.AddDbContextPool<ChinookContext>(options => options.UseSqlite(connection));
         return services;
     }
 }
